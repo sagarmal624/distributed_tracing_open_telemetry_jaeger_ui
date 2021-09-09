c7c801244d6aae27599586427e1f941fbbc5bd8f
sagar.shankhala <sagar.shankhala@publicissapient.com>
2021-09-09 08:47:22 +0530

    first commit

--- opentelemetry-app-a/src/main/java/com/javastaff/tracing/opentelemetrya/AppAController.java
+++ opentelemetry-app-a/src/main/java/com/javastaff/tracing/opentelemetrya/AppAController.java
@@ -2,6 +2,10 @@ package com.javastaff.tracing.opentelemetrya;
 
 import java.util.Arrays;
 
+
+import io.opentelemetry.api.trace.Span;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -12,18 +16,21 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+
 public class AppAController {
-    
+    final static Logger logger = LoggerFactory.getLogger(AppAController.class);
     @Autowired
     private RestTemplate restTemplate;
 
     @RequestMapping("/test-tracing")
     public String entryPointController() {
-    	HttpHeaders headers = new HttpHeaders();
-    	headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-    	HttpEntity<String> entity = new HttpEntity<>(headers);
-    	String response=restTemplate.exchange("http://opentelemetry-app-b:9082/service", HttpMethod.GET, entity, String.class).getBody();	
-    	
-        return "Remote server said: "+response;
+
+        logger.info("hellos");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        String response = restTemplate.exchange("http://opentelemetry-app-b:9082/service", HttpMethod.GET, entity, String.class).getBody();
+
+        return "Remote server said: " + response;
     }
 }
